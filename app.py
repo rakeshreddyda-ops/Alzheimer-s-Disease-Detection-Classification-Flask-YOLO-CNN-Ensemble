02ac0217029fd01794abb01ee79c4d03b0780647
Rakesh <rakeshreddy.da@gmail.com>
2026-05-18 07:43:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,263 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+# Flask utils
+import argparse
+import io
+import os
+from PIL import Image
+import cv2
+import numpy as np
+from torchvision.models import detection
+import sqlite3
+import torch
+from torchvision import models
+from ultralytics import YOLO
+
+from flask import Flask, render_template, request, redirect, send_file, url_for, Response
+from werkzeug.utils import secure_filename, send_from_directory
+import sqlite3
+import pandas as pd
+import numpy as np
+import pickle
+import sqlite3
+import random
+
+import smtplib 
+from email.message import EmailMessage
+from datetime import datetime
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+model = YOLO('best.pt')
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+# allow files of a specific type
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+# function to check the file extension
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model_path1 = 'extension.h5'
+
+CTS1 = load_model(model_path1,  compile=False)
+
+
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+def model_predict1(image_path,model):
+    print("Predicted")
+    image = load_img(image_path,target_size=(128,128))
+    image = img_to_array(image)
+    image = image/255
+    image = np.expand_dims(image,axis=0)
+    
+    result = np.argmax(model.predict(image))
+    print(result)
+    #prediction = classes2[result]  
+    
+    if result == 0:
+        return "PATIENT HAS MILD DEMENTED, BASED ON UPLOAD IMAGE!","result.html"        
+    elif result == 1:
+        return "PATIENT HAS MODERATE DEMENTED, BASED ON UPLOAD IMAGE!","result.html"
+    elif result == 2:
+        return "PATIENT HAS NON DEMENTED, BASED ON UPLOAD IMAGE!","result.html"
+    elif result == 3:
+        return "PATIENT HAS VERY MILD DEMENTED, BASED ON UPLOAD IMAGE!","result.html"
+    
+    
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('home.html')
+
+
+@app.route('/logon')
+def logon():
+	return render_template('signup.html')
+
+@app.route('/login')
+def login():
+	return render_template('signin.html')
+
+
+@app.route('/index1')
+def index1():
+	return render_template('index1.html')
+
+
+@app.route('/index')
+def index():
+	return render_template('index.html')
+
+
+
+@app.route('/predict1',methods=['GET','POST'])
+def predict1():
+    print("Entered")
+    
+    print("Entered here")
+    file = request.files['file'] # fet input
+    filename = file.filename        
+    print("@@ Input posted = ", filename)
+        
+    file_path = os.path.join(UPLOAD_FOLDER, filename)
+    file.save(file_path)
+
+    print("@@ Predicting class......")
+    pred, output_page = model_predict1(file_path,CTS1)
+              
+    return render_template(output_page, pred_output = pred, img_src=UPLOAD_FOLDER + file.filename)
+
+
+@app.route("/signup")
+def signup():
+    global otp, username, name, email, number, password
+    username = request.args.get('user','')
+    name = request.args.get('name','')
+    email = request.args.get('email','')
+    number = request.args.get('mobile','')
+    password = request.args.get('password','')
+    otp = random.randint(1000,5000)
+    print(otp)
+    msg = EmailMessage()
+    msg.set_content("Your OTP is : "+str(otp))
+    msg['Subject'] = 'OTP'
+    msg['From'] = "rakeshreddyddaa@gmail.com"
+    msg['To'] = email
+    
+    
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("rakeshreddyddaa@gmail.com", "fbye hwgn xeva rsfr")
+    s.send_message(msg)
+    s.quit()
+    return render_template("val.html")
+
+@app.route('/predict_lo', methods=['POST'])
+def predict_lo():
+    global otp, username, name, email, number, password
+    if request.method == 'POST':
+        message = request.form['message']
+        print(message)
+        if int(message) == otp:
+            print("TRUE")
+            con = sqlite3.connect('signup.db')
+            cur = con.cursor()
+            cur.execute("insert into `info` (`user`,`email`, `password`,`mobile`,`name`) VALUES (?, ?, ?, ?, ?)",(username,email,password,number,name))
+            con.commit()
+            con.close()
+            return render_template("signin.html")
+    return render_template("signup.html")
+
+@app.route("/signin")
+def signin():
+
+    mail1 = request.args.get('user','')
+    password1 = request.args.get('password','')
+    con = sqlite3.connect('signup.db')
+    cur = con.cursor()
+    cur.execute("select `user`, `password` from info where `user` = ? AND `password` = ?",(mail1,password1,))
+    data = cur.fetchone()
+
+    if data == None:
+        return render_template("signin.html")    
+
+    elif mail1 == str(data[0]) and password1 == str(data[1]):
+        return render_template("index.html")
+    else:
+        return render_template("signin.html")
+
+@app.route("/notebook1")
+def notebook1():
+    return render_template("Classification.html")
+
+@app.route("/notebook2")
+def notebook2():
+    return render_template("Detection.html")
+   
+@app.route("/predict", methods=["GET", "POST"])
+def predict_img():
+    if request.method == "POST":
+        if 'file' in request.files:
+            f = request.files['file']
+            basepath = os.path.dirname(__file__)
+            filepath = os.path.join(basepath,'uploads',f.filename)
+            print("upload folder is ", filepath)
+            f.save(filepath)
+            global imgpath
+            predict_img.imgpath = f.filename
+            print("printing predict_img :::::: ", predict_img)
+                                               
+            file_extension = f.filename.rsplit('.', 1)[1].lower() 
+            
+            if file_extension == 'jpg':
+                img = cv2.imread(filepath)
+
+                # Perform the detection
+                model = YOLO('best.pt')
+                detections =  model(img, save=True) 
+                return display(f.filename)
+            
+            
+
+
+            
+    folder_path = 'runs/detect'
+    subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
+    latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
+    image_path = folder_path+'/'+latest_subfolder+'/'+f.filename 
+    return render_template('index1.html', image_path=image_path)
+    #return "done"
+
+
+
+# #The display function is used to serve the image or video from the folder_path directory.
+@app.route('/<path:filename>')
+def display(filename):
+    folder_path = 'runs/detect'
+    subfolders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f))]    
+    latest_subfolder = max(subfolders, key=lambda x: os.path.getctime(os.path.join(folder_path, x)))    
+    directory = folder_path+'/'+latest_subfolder    
+    print("printing directory: ",directory) 
+    files = os.listdir(directory)
+    latest_file = files[0]
+    
+    print(latest_file)
+
+    filename = os.path.join(folder_path, latest_subfolder, latest_file)
+
+    file_extension = filename.rsplit('.', 1)[1].lower()
+
+    environ = request.environ
+    if file_extension == 'jpg':      
+        return send_from_directory(directory,latest_file,environ) #shows the result in seperate tab
+
+    else:
+        return "Invalid file format"
+
+if __name__ == '__main__':
+    app.run(debug=False)
